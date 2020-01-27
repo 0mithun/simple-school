7bbb3574c8e00a54e76a0bdffaa9c8da6b34c2c4
Mithun Halder <mithun.tutorial.one@gmail.com>
2020-01-27 09:10:08 +0600

    Update user table seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,9 +1,18 @@
 <?php
 
+use App\AppSetup;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
+    /**
+     * @var array
+     */
+
+    
+    
+  
     /**
      * Run the database seeds.
      *
@@ -11,6 +20,24 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class,10)->create();
+        //factory(App\User::class,10)->create();
+        $settings = AppSetup::create([
+            'app_title'    =>   'Simple School',
+            'app_description'    =>   'Simple School Management Application',
+            'copyright_title'    =>   'copyright@techmithun.com',
+            'app_logo'    =>   asset('assets/logo.png'),
+            'app_favicon'    =>   asset('assets/logo.png'),
+        ]);
+
+       $user = User::create([
+        'name' => 'Mithun Halder',
+        'email' => 'halderm86@gmail.com',
+        'password' =>  bcrypt('123456'),
+        'sex'       =>  1,
+        'date_of_birth' =>  now(),
+        'user_photo'    =>  asset('assets/images/img.jpg'),
+        'remember_token' => str_random(10),
+     ]);
+
     }
 }
